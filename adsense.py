e338e2b05c447813183d338245846f357533df92
BlackWizard <BlackWizard@mail.ru>
2013-04-25 17:29:41 +0400

    Dirty fallback to template

--- adsense.py
+++ adsense.py
@@ -3,22 +3,23 @@ from UserDict import UserDict
 from django.utils.http import urlencode
 
 
-def adsense(request, client, ad_type="text_image", channel="", 
+def adsense(request, client, slot, ad_type="text_image", channel="", 
             format="mobile_single", markup="xhtml", oe="utf8", 
             output="xhtml", timeout=1, fail_silently=False):
-    a = AdSense(request, client, ad_type, channel, format, markup, oe, output)
+    a = AdSense(request, client, slot, ad_type, channel, format, markup, oe, output)
     return a.display(timeout, fail_silently)
     
 
 class AdSense(UserDict):
     endpoint = '%s://pagead2.googlesyndication.com/pagead/ads?%s'
 
-    def __init__(self, request, publisher_id, ad_type="text_image", channel="", 
+    def __init__(self, request, publisher_id, slot_id, ad_type="text_image", channel="", 
                  format="mobile_single", markup="xhtml", encoding="utf8", 
                  output="xhtml"):
         self.request = request
         self.data = {
             "client": "ca-mb-%s" % publisher_id,
+            "slotname": slot_id,
             "ad_type": ad_type,
             "channel": channel,
             "format": format,
@@ -31,7 +32,7 @@ class AdSense(UserDict):
             "useragent": self.get_header("USER_AGENT"),
             "url": request.build_absolute_uri()
         }
-        
+
         self.set_screen_size()
         self.set_muid()
         self.set_via_and_accept()
--- templatetags/adsense_tags.py
+++ templatetags/adsense_tags.py
@@ -1,17 +1,20 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django import template
+from django.shortcuts import render
+
 from adsense.adsense import adsense
 
 register = template.Library()
 
 fail_silently = getattr(settings, "ADSENSE_DEBUG", None) or settings.DEBUG
 default_publisher_id = getattr(settings, "ADSENSE_PUBLISHER_ID", None)
-# params = getattr(settings, "ADSENSE_DEFAULTS", None)
+default_slot_id = getattr(settings, "ADSENSE_SLOT_ID", None)
 
 class MobileAdSenseNode(template.Node):
-    def __init__(self, publisher_id):
+    def __init__(self, publisher_id, slot_id):
         self.publisher_id = template.Variable(publisher_id)
+        self.slot_id = template.Variable(slot_id)
 
     def render(self, context):
         assert "request" in context, "AdSense tag requires request in context."
@@ -20,17 +23,29 @@ class MobileAdSenseNode(template.Node):
             publisher_id = self.publisher_id.resolve(context)
         else:
             publisher_id = default_publisher_id
-        if publisher_id is None:
-            raise ImproperlyConfigured("No AdSense publisher id given.")
-                
-        return adsense(context["request"], publisher_id, 
+
+        if self.slot_id is not None:
+            slot_id = self.slot_id.resolve(context)
+        else:
+            slot_id = default_slot_id
+        
+        if publisher_id is None or slot_id is None:
+            raise ImproperlyConfigured("No AdSense publisher or slot id given.")
+
+        res = adsense(context["request"], publisher_id, slot_id,  
                        fail_silently=fail_silently)
+
+        if not res or res == '<!-- google_afm -->':
+            res = render(context["request"], 'adsense.html').content
+
+        return res
 
 @register.tag
 def mobileadsense(parser, token):
     try:
-        tag_name, publisher_id = token.contents.split(None, 1)
+        tag_name, publisher_id, slot_id = token.split_contents() 
     except ValueError:
         publisher_id = None
+        slot_id = None
 
-    return MobileAdSenseNode(publisher_id)
+    return MobileAdSenseNode(publisher_id, slot_id)
